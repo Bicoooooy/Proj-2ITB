240d950fc491da55b96648dc02637fdefb713847
Bicoooooy <angelico.delacruz.iics@ust.edu.ph>
2020-02-16 12:03:37 +0800

    Second commit

---
+++ form.js
@@ -0,0 +1,9 @@
+function billingFunction(){
+
+    var InputVal1 = document.getElementById("shippingName").value;
+    var InputVal2 = document.getElementById("shippingZip").value; 
+
+    billingName.value = InputVal1;
+    billingZip.value = InputVal2;
+    
+}
